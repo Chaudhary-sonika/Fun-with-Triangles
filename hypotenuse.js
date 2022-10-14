3e15c0ea8428474a6f4ed1f551d0e8c3a0603aec
Chaudhary-sonika <111914130+Chaudhary-sonika@users.noreply.github.com>
2022-10-14 21:16:04 +0530

    doc: debug invalid input

--- hypotenuse.js
+++ hypotenuse.js
@@ -1,19 +1,26 @@
 const sides = document.querySelectorAll(".side-input");
 const hypotenuseBtn = document.querySelector("#hypotenuse-btn");
-const outputEl = document.querySelector("#output");
+const output = document.querySelector("#output");
+const error = document.querySelector(".error");
 
-function calculateSumOfSquares(a,b) {
-  const sumOfSquares = a*a + b*b;
-  return sumOfSquares;
+function calculateSumOfSqrOfSide(a,b) {
+  const sumOfSqr = a*a + b*b;
+  return sumOfSqr;
 }
-
-function calculateHypotenuse() {
-    const sumOfSquares = calculateSumOfSquares(
+if(sides[0].value > 0 || sides[1].value > 0) {
+  error.style.display = "none";
+  function calculateHypoOfTriangle() {
+    const sumOfSqr = calculateSumOfSqrOfSide(
         Number(sides[0].value), Number(sides[1].value));
-    const lengthOfHypotenuse = Math.sqrt(sumOfSquares);    
+    const hypotenuse = Math.sqrt(sumOfSqr);    
 
-    outputEl.innerText = "The length of hypotenuse is " + lengthOfHypotenuse + "cm";
+    output.innerText = "The length of hypotenuse is " + hypotenuse + "cm";
+}
+} else {
+error.style.display = "block";
+error.innerText = "Enter the valid input Which is greater than 0.";
 }
 
 
-hypotenuseBtn.addEventListener("click", calculateHypotenuse);
+
+hypotenuseBtn.addEventListener("click", calculateHypoOfTriangle);
